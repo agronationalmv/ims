689905a01958ec02e491eba1711f1118b291248c
Ibrahim Hassaan <ibr.hassaan@gmail.com>
2023-11-28 10:35:23 +0500

    fix: average pricing added

--- app/Filament/Resources/BillResource/Pages/CreateBill.php
+++ app/Filament/Resources/BillResource/Pages/CreateBill.php
@@ -4,12 +4,29 @@ namespace App\Filament\Resources\BillResource\Pages;
 
 use App\Filament\Resources\BillResource;
 use App\Models\Bill;
+use App\Models\BillDetail;
+use App\Models\Product;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class CreateBill extends CreateRecord
 {
     protected static string $resource = BillResource::class;
+
+    protected function afterCreate() : void {
+        foreach($this->record->items as $item){
+            $product=$item->product;
+            $product->price=$this->average_price($product);
+            $product->save();
+        }
+    }
+
+    public function average_price(Product $product){
+        $qty=BillDetail::where('product_id',$product->id)->sum('qty');
+        $total=BillDetail::where('product_id',$product->id)->sum('total');
+        return round($total/$qty,2);
+    }
 }
